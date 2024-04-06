373f38bba3b4ed5566a7258aee68bffff017cb8b
Christian <christianheyer@msn.com>
2024-04-06 16:50:51 +0200

    FInal changes to worksheets

---
+++ Rmd Files/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# INstall following packages:
+sudo apt install libcurl4-openssl-dev
+sudo apt install libxml2-dev
+sudo apt install libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev 
+sudo apt install libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+sudo apt install libproj22 libudunits2-dev libgdal-dev
