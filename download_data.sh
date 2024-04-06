8fbe83e571fae368d8c070d06735818440832965
Christian <36476797+Christian-Heyer@users.noreply.github.com>
2024-04-06 14:48:26 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,3 @@
 # Download the data to directory /vol/data/course_data
 #mkdir -p /vol/data/course_data
 cd /vol/data/course_data
-# Download Link https://ucd0bc5d0894b63309c5bd3bf4a9.dl.dropboxusercontent.com/zip_download_get/Bz1Xgff1gNq25TQffAgQviUWqc-hhozXJ4sXBdj4-2PvfiKzVdS52z-U4ujowlzv8te6n52reOuFoT4ury00zj9vhAUN3P0rFqjnogICggtVbQ?_download_id=31470521775618547772238909810365047511645138911839484230019146&_notify_domain=www.dropbox.com&dl=1
-wget -O data.zip https://ucd0bc5d0894b63309c5bd3bf4a9.dl.dropboxusercontent.com/zip_download_get/Bz1Xgff1gNq25TQffAgQviUWqc-hhozXJ4sXBdj4-2PvfiKzVdS52z-U4ujowlzv8te6n52reOuFoT4ury00zj9vhAUN3P0rFqjnogICggtVbQ?_download_id=31470521775618547772238909810365047511645138911839484230019146&_notify_domain=www.dropbox.com&dl=1
-unzip data.zip
